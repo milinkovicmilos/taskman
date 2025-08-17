250be0dd8d4b67ecc710ac652e564bb5e06ee20e
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-08-17 15:25:27 +0200

    CORS config for SPA. API endpoints for loggin in and logging out the user.

---
+++ server/app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return response()->json(['message' => 'Successfully authenticated.']);
+        }
+
+        return response()->json(['message' => 'Failed to authenticate with provided credentials.'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return response()->json(['message' => 'Successfully logged out.']);
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
