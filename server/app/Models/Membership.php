405888b1c5293d88c3dfa781724b5507288c186f
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-08-18 12:49:45 +0200

    Membership model, migration. Role and membership relationships.

---
+++ server/app/Models/Membership.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Group;
+use App\Models\Role;
+
+class Membership extends Model
+{
+    /** @use HasFactory<\Database\Factories\MembershipFactory> */
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+}
