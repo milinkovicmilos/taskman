5fb1f4a5dba1731e691b6b78eab4118f3365437a
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-08-18 13:02:11 +0200

    Group model, migration and relationships

--- server/app/Models/Group.php
+++ server/app/Models/Group.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
+use App\Models\Membership;
 
 class Group extends Model
 {
     /** @use HasFactory<\Database\Factories\GroupFactory> */
     use HasFactory;
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function memberships()
+    {
+        return $this->hasMany(Membership::class);
+    }
 }
