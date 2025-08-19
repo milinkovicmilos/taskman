cc58b2c5134a4674af49d8aa8772216ad3c0a296
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-08-19 14:14:35 +0200

    Seeding demo for user and group projects

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
+use App\Models\Membership;
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,10 +21,48 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
         ]);
 
-        User::factory()->create([
-            'first_name' => 'John',
-            'last_name' => 'Doe',
-            'email' => 'test@example.com',
+        User::factory(5)->create();
+
+        Group::factory()->create([
+            'name' => fake()->company(),
         ]);
+
+        Membership::factory()->create([
+            'user_id' => 1,
+            'group_id' => 1,
+            'role_id' => 1,
+        ]);
+
+        Membership::factory()->create([
+            'user_id' => 2,
+            'group_id' => 1,
+            'role_id' => 2,
+        ]);
+
+        Project::factory()->create([
+            'user_id' => 1,
+            'group_id' => 1,
+            'name' => fake()->text(12),
+        ]);
+
+        Project::factory()->create([
+            'user_id' => 1,
+            'group_id' => 1,
+            'name' => fake()->text(12),
+        ]);
+
+        for ($i = 0; $i < 5; $i++) {
+            Project::factory()->create([
+                'user_id' => 1,
+                'name' => fake()->text(12),
+            ]);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            Project::factory()->create([
+                'user_id' => 2,
+                'name' => fake()->text(12),
+            ]);
+        }
     }
 }
