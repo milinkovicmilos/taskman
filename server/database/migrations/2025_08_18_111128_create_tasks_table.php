186dae70f25b792669269eec61dd0fb586eba113
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-08-21 15:02:20 +0200

    Changed Task schema to include the user that made the task

--- server/database/migrations/2025_08_18_111128_create_tasks_table.php
+++ server/database/migrations/2025_08_18_111128_create_tasks_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,6 +15,7 @@ return new class () extends Migration {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Project::class);
+            $table->foreignIdFor(User::class);
             $table->string('title');
             $table->string('description');
             $table->date('due_date')->nullable();
