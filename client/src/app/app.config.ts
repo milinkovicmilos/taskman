ff226b8cd2e29241a1a47742b97bad81e32b1b17
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-02 23:27:32 +0200

    Provide http client. Use XSRF cookie and credentials interceptor.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,11 +2,20 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
+import { addCredentialsInterceptor } from './core/add-credentials-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      }),
+      withInterceptors([addCredentialsInterceptor]),
+    ),
   ]
 };
