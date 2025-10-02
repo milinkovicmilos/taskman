3f1eda4e71aaadbc260232fdd98dcda6d775da3b
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-02 23:33:34 +0200

    Login form submission handling

--- client/src/app/features/login/components/login/login.ts
+++ client/src/app/features/login/components/login/login.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoginForm } from '../login-form/login-form';
 import { RouterModule } from '@angular/router';
+import { LoginData } from '../../interfaces/login-data';
+import { AuthService } from '../../../../shared/services/auth-service';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +11,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './login.css'
 })
 export class Login {
+  authService = inject(AuthService);
 
+  onLoginSubmit(data: LoginData) {
+    this.authService.login(data).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
