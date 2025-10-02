8e50b15fb7f1cabf347bec428b5a68f550574b97
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-02 23:32:03 +0200

    Auth service login function

--- client/src/app/shared/services/auth-service.ts
+++ client/src/app/shared/services/auth-service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { LoginData } from '../../features/login/interfaces/login-data';
+import { Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,14 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   // To be changed
   isLoggedIn: boolean = false;
+
+  private http = inject(HttpClient);
+
+  login(data: LoginData): Observable<any> {
+    return this.http.get('sanctum/csrf-cookie').pipe(
+      switchMap(() =>
+        this.http.post<LoginData>('api/login', data)
+      )
+    );
+  }
 }
