c3e3ca8d1802d410fb4bf6bd0f963d52b3dfa2de
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-02 19:33:41 +0200

    Login navigation

--- client/src/app/shared/components/header/header.ts
+++ client/src/app/shared/components/header/header.ts
@@ -1,17 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { Button } from '../button/button';
 import { FormState } from '../../services/form-state';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [Button],
+  imports: [RouterModule, Button],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  private router = inject(Router);
+
   state = inject(FormState);
 
   toggleNewProjectForm() {
     this.state.changeState();
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
   }
 }
