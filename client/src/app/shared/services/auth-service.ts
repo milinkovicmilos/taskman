cd2e7a08131c90f056a614020dd9c9302455a8a7
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-03 19:09:18 +0200

    Auth service register function

--- client/src/app/shared/services/auth-service.ts
+++ client/src/app/shared/services/auth-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, WritableSignal, inject, signal } from '@angular/core';
 import { LoginData } from '../../features/login/interfaces/login-data';
 import { firstValueFrom, Observable, pipe, switchMap, tap } from 'rxjs';
+import { RegisterData } from '../../features/register/interfaces/register-data';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,12 @@ export class AuthService {
   private http = inject(HttpClient);
 
   isLoggedIn: WritableSignal<boolean> = signal(false);
+
+  register(data: RegisterData) {
+    return this.http.post('api/register', data).pipe(
+      tap(() => this.isLoggedIn.set(true))
+    );
+  }
 
   async checkIfLoggedIn(): Promise<void> {
     return firstValueFrom(this.http.get('api/user'))
