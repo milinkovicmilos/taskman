ed5af7561cd741a99a4bb155d5f3ce89eda40353
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-03 20:08:35 +0200

    Getting the server errors and displaying them

--- client/src/app/features/register/components/register-form/register-form.ts
+++ client/src/app/features/register/components/register-form/register-form.ts
@@ -1,17 +1,18 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, WritableSignal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegisterData } from '../../interfaces/register-data';
 import { passwordConfirmValidator } from '../../../../shared/validators/password-confirm-validator';
-import { Input } from '../../../../shared/components/input/input';
+import { Input as InputElement } from '../../../../shared/components/input/input';
 import { Button } from '../../../../shared/components/button/button';
 import { symbolValidator } from '../../../../shared/validators/symbol-validator';
 import { lowerCaseValidator } from '../../../../shared/validators/lower-case-validator';
 import { upperCaseValidator } from '../../../../shared/validators/upper-case-validator';
 import { numberCaseValidator } from '../../../../shared/validators/number-validator';
+import { ServerError } from '../../../../shared/interfaces/server-error';
 
 @Component({
   selector: 'app-register-form',
-  imports: [ReactiveFormsModule, Input, Button],
+  imports: [ReactiveFormsModule, InputElement, Button],
   templateUrl: './register-form.html',
   styleUrl: './register-form.css'
 })
@@ -32,28 +33,42 @@ export class RegisterForm {
   protected password = this.registerForm.get('password');
   protected confirmPassword = this.registerForm.get('confirmPassword');
 
+  @Input() serverErrors!: WritableSignal<ServerError[]>;
+
   @Output() submitted = new EventEmitter<RegisterData>();
+
+  getServerErrors(propertyName: string): string[] {
+    for (const error of this.serverErrors()) {
+      if (error.property == propertyName) {
+        return error.errors;
+      }
+    }
+
+    return [];
+  }
 
   handleSubmit(): void {
     this.isSubmitted = true;
+    this.serverErrors.set([]);
     if (this.registerForm.valid) {
-      const { first_name, last_name, email, password, password_confirmation } = this.registerForm.value as {
-        first_name: string;
-        last_name: string;
+      const { firstName, lastName, email, password, confirmPassword } = this.registerForm.value as {
+        firstName: string;
+        lastName: string;
         email: string,
         password: string,
-        password_confirmation: string;
+        confirmPassword: string;
       };
 
       const register: RegisterData = {
-        first_name,
-        last_name,
+        first_name: firstName,
+        last_name: lastName,
         email,
         password,
-        password_confirmation,
+        password_confirmation: confirmPassword,
       };
 
       this.isSubmitted = false;
+      this.registerForm.markAsUntouched();
       this.submitted.emit(register);
     }
   }
