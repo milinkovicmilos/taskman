2ddc365404859caa79a03659794cc72c886bf543
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-03 18:24:16 +0200

    Register page component

--- client/src/app/features/register/components/register/register.ts
+++ client/src/app/features/register/components/register/register.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RegisterForm } from '../register-form/register-form';
+import { RegisterData } from '../../interfaces/register-data';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterModule, RegisterForm],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
-
+  onRegisterSubmit(data: RegisterData) {
+    // ...
+  }
 }
