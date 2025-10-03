f34ef354305d9094287f070d24c0a42633088d34
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-03 17:13:28 +0200

    Routing for home, register, login and logout pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/components/home/home';
+import { Register } from './features/register/components/register/register';
+import { Login } from './features/login/components/login/login';
+import { Logout } from './features/logout/components/logout/logout';
+import { Project } from './features/projects/components/project/project';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: 'logout', component: Logout },
+  { path: 'projects', component: Project },
+];
