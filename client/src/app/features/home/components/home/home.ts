1e6d8c1d057d6812ae843942ccbf1423b11607eb
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-03 17:12:24 +0200

    Initial home page

---
+++ client/src/app/features/home/components/home/home.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { Button } from '../../../../shared/components/button/button';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [Button],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+  private router: Router = inject(Router);
+
+  goToProjects(): void {
+    this.router.navigate(['/projects']);
+  }
+}
