a4e4dfd8d28cebf2f7f35b538f9a2257998b22e8
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-03 20:11:09 +0200

    Using the auth service register method. Fetching the validation errors from server and providing them.

--- client/src/app/features/register/components/register/register.ts
+++ client/src/app/features/register/components/register/register.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { RegisterForm } from '../register-form/register-form';
 import { RegisterData } from '../../interfaces/register-data';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../../shared/services/auth-service';
+import { ServerError } from '../../../../shared/interfaces/server-error';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,32 @@ import { RouterModule } from '@angular/router';
   styleUrl: './register.css'
 })
 export class Register {
-  onRegisterSubmit(data: RegisterData) {
-    // ...
+  authService = inject(AuthService);
+  private router = inject(Router);
+
+  protected serverErrors: WritableSignal<ServerError[]> = signal([]);
+
+  onRegisterSubmit(data: RegisterData): void {
+    this.authService.register(data).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        this.serverErrors = signal([]);
+        const errs: ServerError[] = [];
+        const errorsObj = error.error.errors;
+        for (const propertyName in errorsObj) {
+          const errList = [];
+          for (const error of errorsObj[propertyName]) {
+            errList.push(error)
+          }
+          errs.push({
+            property: propertyName,
+            errors: errList,
+          });
+        }
+        this.serverErrors.set(errs);
+      }
+    });
   }
 }
