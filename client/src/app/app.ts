7da592180cc342fb2f9e54ca14cec4bbfb71f299
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-04 13:33:18 +0200

    Displaying notifications

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { Notifier } from './shared/services/notifier';
+import { Notification } from './shared/components/notification/notification';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Notification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('client');
+  protected readonly notification = inject(Notifier).notification;
 }
