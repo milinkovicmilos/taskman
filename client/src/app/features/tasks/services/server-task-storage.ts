4f25f7d030d7bcad4cceabda9cb71a5604456f97
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-04 20:11:35 +0200

    Fetching all projects tasks from server

--- client/src/app/features/tasks/services/server-task-storage.ts
+++ client/src/app/features/tasks/services/server-task-storage.ts
@@ -2,6 +2,8 @@ import { Injectable, inject } from '@angular/core';
 import { TaskStorage } from '../interfaces/task-storage';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { PaginatedResponse } from '../../../shared/interfaces/paginated-response';
+import { TaskData } from '../interfaces/task-data';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +11,10 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class ServerTaskStorage implements TaskStorage {
   private http = inject(HttpClient);
 
-  getTasks(projectId: number | string, page: number = 1) {
+  getTasks(projectId: number | string, page: number = 1): Observable<PaginatedResponse<TaskData>> {
     let params = new HttpParams();
     params = params.append('page', page);
-    return this.http.get(`api/projects/${projectId}/tasks`, { params: params });
+    return this.http.get<PaginatedResponse<TaskData>>(`api/projects/${projectId}/tasks`, { params: params });
   }
 
   getTask(projectId: number | string, taskId: number | string): Observable<any> {
