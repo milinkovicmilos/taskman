22ea8522d56da42ff238f7664a1cb356cd3f842a
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-04 21:23:26 +0200

    Due date text styling

--- client/src/app/features/tasks/components/task-card/task-card.ts
+++ client/src/app/features/tasks/components/task-card/task-card.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { TaskData } from '../../interfaces/task-data';
+import { DueDatePipe } from '../../pipes/due-date-pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [DueDatePipe, CommonModule],
   templateUrl: './task-card.html',
   styleUrl: './task-card.css'
 })
 export class TaskCard {
   @Input() task!: TaskData;
+
+  protected getDueDateClass(dueDate: string): string {
+    const now = new Date();
+    const date = new Date(dueDate);
+
+    return now >= date ? 'late' : 'due';
+  }
 }
