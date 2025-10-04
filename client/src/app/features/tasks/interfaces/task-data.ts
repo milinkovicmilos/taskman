f9f573716628528ae3622f0d81055a139ca6dba4
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-04 20:11:11 +0200

    Interface describing task data

---
+++ client/src/app/features/tasks/interfaces/task-data.ts
@@ -0,0 +1,9 @@
+export interface TaskData {
+  id: number | string;
+  title: string;
+  description: string;
+  priority: number | null;
+  due_date: string | null;
+  completed: boolean;
+  completed_at: string | null;
+}
