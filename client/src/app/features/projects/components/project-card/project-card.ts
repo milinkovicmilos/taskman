6651e74dfd1d26264e21d050b16745006b004ff5
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-04 18:33:23 +0200

    Removed project delete button from project listing page

--- client/src/app/features/projects/components/project-card/project-card.ts
+++ client/src/app/features/projects/components/project-card/project-card.ts
@@ -1,23 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Button } from '../../../../shared/components/button/button';
 import { ProjectData } from '../../interfaces/project-data';
 
 @Component({
   selector: 'app-project-card',
-  imports: [Button],
+  imports: [],
   templateUrl: './project-card.html',
   styleUrl: './project-card.css'
 })
 export class ProjectCard {
   @Input() project!: ProjectData;
 
-  @Output() deleted: EventEmitter<number | string> = new EventEmitter<number | string>();
-
   onClick() {
     // Project clicked
-  }
-
-  onDeleteClick() {
-    this.deleted.emit(this.project.id);
   }
 }
