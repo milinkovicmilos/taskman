c9183f5ee9c7aa8a0f9e846f39aefd75ddfb6a67
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-04 19:45:22 +0200

    Displaying specific projects details

--- client/src/app/features/projects/components/project-detail/project-detail.ts
+++ client/src/app/features/projects/components/project-detail/project-detail.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { ProjectDetailData } from '../../interfaces/project-data';
+import { PROJECT_STORAGE } from '../../interfaces/project-storage';
+import { AuthService } from '../../../../shared/services/auth-service';
+import { ServerProjectStorage } from '../../services/server-project-storage';
+import { LocalProjectStorage } from '../../services/local-project-storage';
+import { ProjectRole } from '../../enums/project-role';
+import { Button } from '../../../../shared/components/button/button';
 
 @Component({
   selector: 'app-project-detail',
-  imports: [],
+  imports: [Button],
   templateUrl: './project-detail.html',
-  styleUrl: './project-detail.css'
+  styleUrl: './project-detail.css',
+  providers: [
+    {
+      provide: PROJECT_STORAGE,
+      useFactory: (() => {
+        const authService = inject(AuthService);
+        return authService.isLoggedIn() ? new ServerProjectStorage() : new LocalProjectStorage();
+      }),
+    },
+  ],
 })
-export class ProjectDetail {
+export class ProjectDetail implements OnInit {
+  private storage = inject(PROJECT_STORAGE);
+  protected project!: ProjectDetailData;
+  protected projectRoles = ProjectRole;
 
+  @Input() protected id!: number | string;
+
+  ngOnInit(): void {
+    this.storage.getProject(this.id).subscribe({
+      next: (response) => {
+        this.project = response;
+      }
+    });
+  }
 }
