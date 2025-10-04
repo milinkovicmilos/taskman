c9fe8f1a11661d60535f1de8c58d94cb87956e9e
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-04 19:44:29 +0200

    Data to define the users role on project

--- client/src/app/features/projects/interfaces/project-data.ts
+++ client/src/app/features/projects/interfaces/project-data.ts
@@ -1,5 +1,11 @@
+import { ProjectRole } from "../enums/project-role";
+
 export interface ProjectData {
   id: number | string;
   name: string;
   description: string;
+}
+
+export interface ProjectDetailData extends ProjectData {
+  role: ProjectRole;
 }
