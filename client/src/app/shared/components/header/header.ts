05f6964225bded688b3fdf93528e79b4305f2058
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-05 19:21:51 +0200

    Header button margins. Added back button to header. New project and task button depending on current url.

--- client/src/app/shared/components/header/header.ts
+++ client/src/app/shared/components/header/header.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Button } from '../button/button';
 import { FormState } from '../../services/form-state';
-import { RouterModule, Router } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 import { FormType } from '../../enums/form-type';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -13,12 +14,23 @@ import { FormType } from '../../enums/form-type';
 })
 export class Header {
   private router = inject(Router);
+  protected url = signal(this.router.url);
 
   authService = inject(AuthService);
   isLoggedIn = this.authService.isLoggedIn;
 
   userData = this.authService.data;
   state = inject(FormState);
+
+  constructor() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe({
+        next: (event: NavigationEnd) => {
+          this.url.set(event.urlAfterRedirects);
+        }
+      });
+  }
 
   getFullName(): string {
     return `${this.userData().firstName} ${this.userData().lastName}`;
@@ -38,5 +50,27 @@ export class Header {
 
   goToLogout(): void {
     this.router.navigate(['/logout']);
+  }
+
+  protected isProductsPage(): boolean {
+    const regex = /^\/projects$/;
+    return regex.test(this.router.url);
+  }
+
+  protected isProductDetailPage(): boolean {
+    // UUID REGEX: [0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}
+    const regex = /^\/projects\/(\d+|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/;
+    return regex.test(this.router.url);
+  }
+
+  protected goBack(): void {
+    const url = this.url().split('/');
+    if (url.length > 1) {
+      url.pop();
+      this.router.navigate([`/${url.join('/')}`]);
+    }
+    else {
+      this.router.navigate(['/']);
+    }
   }
 }
