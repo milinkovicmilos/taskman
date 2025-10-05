972a02550465f972bd01e14831e4f4b69a69994a
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-05 16:35:39 +0200

    Using modal appropriately in app component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,16 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Notifier } from './shared/services/notifier';
 import { Notification } from './shared/components/notification/notification';
-import { Modal } from './shared/services/modal';
+import { Modal } from './shared/components/modal/modal';
+import { Modal as ModalService } from './shared/services/modal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Notification],
+  imports: [RouterOutlet, Header, Notification, Modal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('client');
   protected readonly notification = inject(Notifier).notification;
-  protected readonly modal = inject(Modal);
+  protected readonly modal = inject(ModalService);
 }
