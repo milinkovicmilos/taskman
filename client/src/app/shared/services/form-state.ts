0d5b106878a292cee757bd7cbb06747efbf3b154
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-05 20:24:04 +0200

    Cancel form state when changing paths

--- client/src/app/shared/services/form-state.ts
+++ client/src/app/shared/services/form-state.ts
@@ -1,10 +1,23 @@
-import { Injectable, signal, WritableSignal } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormState {
   visible: WritableSignal<number | null> = signal(null);
+  private router = inject(Router);
+
+  constructor() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe({
+        next: () => {
+          this.visible.set(null);
+        }
+      });
+  }
 
   changeState(state: number | null): void {
     if (this.visible() === state) {
