68bd24b02162bacd58de382785818c489b3b711b
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-05 13:37:25 +0200

    Task storing method

--- client/src/app/features/tasks/interfaces/task-storage.ts
+++ client/src/app/features/tasks/interfaces/task-storage.ts
@@ -1,10 +1,11 @@
 import { InjectionToken } from "@angular/core";
 import { Observable } from "rxjs";
+import { CreateTaskData } from "./create-task-data";
 
 export interface TaskStorage {
   getTasks(projectId: number | string, page?: number): Observable<any | null>;
   getTask(projectId: number | string, taskId: number | string): Observable<any | null>;
-  // storeTasks(project: CreateTaskData): Observable<any | null>;
+  storeTask(projectId: number | string, task: CreateTaskData): Observable<any | null>;
   // updateTask(projectId: number | string, project: TaskData): void;
   // removeTask(projectId: number | string): void;
 }
