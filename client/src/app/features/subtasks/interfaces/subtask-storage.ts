34904fd832f6d17684c024f077f56114aac5fcdf
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-06 20:57:35 +0200

    Add method for storing subtasks

--- client/src/app/features/subtasks/interfaces/subtask-storage.ts
+++ client/src/app/features/subtasks/interfaces/subtask-storage.ts
@@ -1,10 +1,11 @@
 import { InjectionToken } from "@angular/core";
 import { Observable } from "rxjs";
+import { CreateSubtaskData } from "./create-subtask-data";
 
 export interface SubtaskStorage {
   getSubtasks(projectId: number | string, taskId: number | string, page?: number): Observable<any | null>;
   // getSubtask(projectId: number | string, taskId: number | string, subtaskid: number | string): Observable<any | null>;
-  // storeSubtask(projectId: number | string, taskId: number | string, subtask: CreateSubtaskData): Observable<any | null>;
+  storeSubtask(projectId: number | string, taskId: number | string, subtask: CreateSubtaskData): Observable<any | null>;
   // updateSubtask(projectId: number | string, taskId: number | string, subtask: SubtaskData): Observable<any | null>;
   // removeSubtask(projectId: number | string, ): Observable<any | null>;
 }
