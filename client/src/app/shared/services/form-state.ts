71a5410a3ec005ba09a689034caa52d86186f485
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-06 13:31:33 +0200

    Allow null to be passed into state to cancel the form

--- client/src/app/shared/services/form-state.ts
+++ client/src/app/shared/services/form-state.ts
@@ -1,12 +1,13 @@
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { FormType } from '../enums/form-type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormState {
-  visible: WritableSignal<number | null> = signal(null);
+  visible: WritableSignal<FormType | null> = signal(null);
   private router = inject(Router);
 
   constructor() {
@@ -19,7 +20,7 @@ export class FormState {
       });
   }
 
-  changeState(state: number | null): void {
+  changeState(state: FormType | null): void {
     if (this.visible() === state) {
       this.visible.set(null);
       return;
