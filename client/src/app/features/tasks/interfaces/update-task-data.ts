0c9b92b81ce56b4af26fee8859f4a9e74a10dcaa
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-06 18:30:39 +0200

    Add optional to every field to match server

--- client/src/app/features/tasks/interfaces/update-task-data.ts
+++ client/src/app/features/tasks/interfaces/update-task-data.ts
@@ -1,6 +1,6 @@
 export interface UpdateTaskData {
-  title: string;
-  description: string;
-  priority: number | null;
-  due_date: string | null;
+  title?: string;
+  description?: string;
+  priority?: number | null;
+  due_date?: string | null;
 }
