94f2098b4c00eda371298b02cc7151481d1a7eaa
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-07 14:51:55 +0200

    Trigger requests for marking subtask complete or incomplete

--- client/src/app/features/subtasks/components/subtask-card/subtask-card.ts
+++ client/src/app/features/subtasks/components/subtask-card/subtask-card.ts
@@ -2,20 +2,70 @@ import { Component, inject, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SubtaskData } from '../../interfaces/subtask-data';
 import { InputCheckbox } from '../../../../shared/components/input-checkbox/input-checkbox';
+import { SUBTASK_STORAGE } from '../../interfaces/subtask-storage';
+import { AuthService } from '../../../../shared/services/auth-service';
+import { ServerSubtaskStorage } from '../../services/server-subtask-storage';
+import { LocalSubtaskStorage } from '../../services/local-subtask-storage';
+import { Notifier } from '../../../../shared/services/notifier';
+import { NotificationType } from '../../../../shared/enums/notification-type';
+import { MessageResponse } from '../../../../shared/interfaces/message-response';
 
 @Component({
   selector: 'app-subtask-card',
   imports: [InputCheckbox],
   templateUrl: './subtask-card.html',
-  styleUrl: './subtask-card.css'
+  styleUrl: './subtask-card.css',
+  providers: [
+    {
+      provide: SUBTASK_STORAGE,
+      useFactory: (() => {
+        const authService = inject(AuthService);
+        return authService.isLoggedIn() ? new ServerSubtaskStorage() : new LocalSubtaskStorage();
+      }),
+    },
+  ]
 })
 export class SubtaskCard {
-  private router = inject(Router);
-  private route = inject(ActivatedRoute);
+  private storage = inject(SUBTASK_STORAGE);
+  private notificationService = inject(Notifier);
+
+  @Input() projectId!: number | string;
+  @Input() taskId!: number | string;
 
   @Input() subtask!: SubtaskData;
 
-  protected onClick(): void {
-    this.router.navigate(['subtasks', this.subtask.id], { relativeTo: this.route });
+  protected onCheckboxChange(value: boolean): void {
+    if (value) {
+      this.storage.markSubtaskComplete(this.projectId, this.taskId, this.subtask.id).subscribe({
+        next: (response: MessageResponse) => {
+          this.notificationService.notify({
+            type: NotificationType.Info,
+            message: response.message,
+          });
+        },
+        error: (response) => {
+          this.notificationService.notify({
+            type: NotificationType.Error,
+            message: response.error.message,
+          });
+        }
+      });
+    }
+    else {
+      this.storage.markSubtaskIncomplete(this.projectId, this.taskId, this.subtask.id).subscribe({
+        next: (response: MessageResponse) => {
+          this.notificationService.notify({
+            type: NotificationType.Info,
+            message: response.message,
+          });
+        },
+        error: (response) => {
+          this.notificationService.notify({
+            type: NotificationType.Error,
+            message: response.error.message,
+          });
+        }
+      });
+    }
   }
 }
