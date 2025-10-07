a3c989627bae89902761abf047ba8de69878432c
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-08 01:04:04 +0200

    Styling, showing group members

--- client/src/app/features/groups/interfaces/group-detail-data.ts
+++ client/src/app/features/groups/interfaces/group-detail-data.ts
@@ -2,6 +2,8 @@ export interface GroupDetailData {
   id: number | string;
   name: string;
   owner: string;
+  members: string[];
+  member_count: number;
   deletable: boolean;
   editable: boolean;
   can_invite_to_group: boolean;
