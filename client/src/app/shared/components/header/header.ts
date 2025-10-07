f336f7bf0c06ac6d7294f1198ba737c026db3a9a
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-07 23:38:17 +0200

    Import and export data button functionality

--- client/src/app/shared/components/header/header.ts
+++ client/src/app/shared/components/header/header.ts
@@ -4,6 +4,7 @@ import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 import { filter } from 'rxjs';
 import { HeaderButton } from '../../services/header-button';
+import { LocalStorage } from '../../services/local-storage';
 
 @Component({
   selector: 'app-header',
@@ -59,5 +60,45 @@ export class Header {
 
     const url = this.urls.pop();
     this.router.navigate([url]);
+  }
+
+  protected importData(): void {
+    const storage = new LocalStorage();
+
+    const inputFile = document.createElement("input");
+    inputFile.setAttribute("type", "file")
+    inputFile.click();
+
+    inputFile.addEventListener('change', () => {
+      if (inputFile.files != null) {
+
+        const file = inputFile.files[0];
+
+        const reader = new FileReader();
+        reader.readAsText(file);
+
+        reader.addEventListener('load', () => {
+          const data = reader.result?.toString();
+
+          if (data != null) {
+            storage.write(JSON.parse(data));
+          }
+        });
+      }
+    });
+  }
+
+  protected exportData(): void {
+    const storage = new LocalStorage();
+
+    const data = storage.fetch();
+
+    const blob = new Blob([data], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = "taskman-export.json";
+    anchor.click();
   }
 }
