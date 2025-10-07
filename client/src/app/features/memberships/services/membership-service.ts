1a83981873f733e0020f78267d81aae71b5ce0c4
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-07 21:23:03 +0200

    Membership fetching, removing, updating

--- client/src/app/features/memberships/services/membership-service.ts
+++ client/src/app/features/memberships/services/membership-service.ts
@@ -1,8 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MessageResponse } from '../../../shared/interfaces/message-response';
 import { CreateMembershipData } from '../interfaces/create-membership-data';
+import { PaginatedResponse } from '../../../shared/interfaces/paginated-response';
+import { MembershipData } from '../interfaces/membership-data';
+import { UpdateMembershipData } from '../interfaces/update-membership-data';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,21 @@ import { CreateMembershipData } from '../interfaces/create-membership-data';
 export class MembershipService {
   private http = inject(HttpClient);
 
+  getUsers(groupId: number | string, page: number = 1): Observable<PaginatedResponse<MembershipData>> {
+    let params = new HttpParams();
+    params = params.append('page', page);
+    return this.http.get<PaginatedResponse<MembershipData>>(`api/groups/${groupId}/memberships`, { params: params });
+  }
+
   inviteUser(groupId: number | string, membership: CreateMembershipData): Observable<MessageResponse> {
     return this.http.post<MessageResponse>(`api/groups/${groupId}/memberships`, membership);
+  }
+
+  updateMembership(groupId: number | string, membershipId: number | string, membership: UpdateMembershipData): Observable<MessageResponse> {
+    return this.http.patch<MessageResponse>(`api/groups/${groupId}/memberships/${membershipId}`, membership);
+  }
+
+  removeMembership(groupId: number | string, membershipId: number | string): Observable<MessageResponse> {
+    return this.http.delete<MessageResponse>(`api/groups/${groupId}/memberships/${membershipId}`);
   }
 }
