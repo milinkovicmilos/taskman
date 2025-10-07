6d5ff12e72a0a572a4c790bf880b7c36cdb1ea99
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-07 14:24:30 +0200

    Show subtask completion

--- client/src/app/features/subtasks/components/subtask-card/subtask-card.ts
+++ client/src/app/features/subtasks/components/subtask-card/subtask-card.ts
@@ -1,10 +1,11 @@
 import { Component, inject, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SubtaskData } from '../../interfaces/subtask-data';
+import { InputCheckbox } from '../../../../shared/components/input-checkbox/input-checkbox';
 
 @Component({
   selector: 'app-subtask-card',
-  imports: [],
+  imports: [InputCheckbox],
   templateUrl: './subtask-card.html',
   styleUrl: './subtask-card.css'
 })
