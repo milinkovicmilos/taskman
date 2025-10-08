36a67c8c8216f51a96b217c73390f2e8764057e7
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-08 13:10:07 +0200

    Changed handling of current page number to allow parent components to manipulate current page

--- client/src/app/shared/components/page-navigation/page-navigation.ts
+++ client/src/app/shared/components/page-navigation/page-navigation.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, WritableSignal } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal, WritableSignal } from '@angular/core';
 import { Button } from '../button/button';
 
 @Component({
@@ -8,25 +8,29 @@ import { Button } from '../button/button';
   styleUrl: './page-navigation.css'
 })
 export class PageNavigation {
-  @Input() lastPage!: WritableSignal<number>;
+  @Input() lastPage: WritableSignal<number> = signal(1);
 
-  protected pageNumber: number = 1;
+  @Input() pageNumber: WritableSignal<number> = signal(1);
 
   @Output() pageChanged: EventEmitter<number> = new EventEmitter<number>();
 
   nextPage(): void {
-    if (this.pageNumber === this.lastPage()) {
+    if (this.pageNumber() === this.lastPage()) {
       return;
     }
 
-    this.pageChanged.emit(++this.pageNumber);
+    const newPage = this.pageNumber() + 1;
+    this.pageNumber.set(newPage);
+    this.pageChanged.emit(this.pageNumber());
   }
 
   previousPage(): void {
-    if (this.pageNumber === 1) {
+    if (this.pageNumber() === 1) {
       return;
     }
 
-    this.pageChanged.emit(--this.pageNumber);
+    const newPage = this.pageNumber() - 1;
+    this.pageNumber.set(newPage);
+    this.pageChanged.emit(this.pageNumber());
   }
 }
