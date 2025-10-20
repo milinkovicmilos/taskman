5e7a541f6f293ff96b4607531d226830b84157c7
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2025-10-20 15:38:06 +0200

    Read stateful domain from env file

--- server/config/cors.php
+++ server/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => [env('SANCTUM_STATEFUL_DOMAINS')],
 
     'allowed_origins_patterns' => [],
 
